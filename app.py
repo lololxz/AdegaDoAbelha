0be137a14cef6f9fe2a36641c651539fa4a559d1
Lorenzo Fogliano <148654988+lololxz@users.noreply.github.com>
2025-03-02 21:41:02 -0300

    Changes

--- app.py
+++ app.py
@@ -29,32 +29,3 @@ def termos():
 if __name__ == '__main__':
     app.run(debug=True)
 from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/carrinho')
-def carrinho():
-    return render_template('carrinho.html')
-
-@app.route('/pagamento')
-def pagamento():
-    return render_template('pagamento.html')
-
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
-
-@app.route('/quem-somos')
-def quem_somos():
-    return render_template('quem_somos.html')
-
-@app.route('/termos')
-def termos():
-    return render_template('termos.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
